1217acccb25f2e6103d3e1cfac056086dbe6efc0
dk872 <143122117+dk872@users.noreply.github.com>
2024-01-14 16:23:17 +0200

    replaced if...else construction, 'max = min' moved to parameters

--- Tasks/random.js
+++ Tasks/random.js
@@ -2,13 +2,8 @@
 
 'use strict';
 
-const Random = (min, max) => {
-  if (max === undefined) {
-    max = min;
-    return Math.floor(Math.random() * (max + 1));
-  } else {
-    return min + Math.floor(Math.random() * (max - min + 1));
-  }
+const Random = (min, max = min) => {
+  return min + Math.floor(Math.random() * (max - min + 1));
 };
 
 module.exports = Random;
