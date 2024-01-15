5ea4b71647339a0c980baf7fc9c09ed016c30991
dk872 <143122117+dk872@users.noreply.github.com>
2024-01-15 10:39:39 +0200

    'max = min' moved to parameters

--- Tasks/random.js
+++ Tasks/random.js
@@ -2,9 +2,8 @@
 
 'use strict';
 
-const Random = (min, max) => {
+const Random = (min, max = min) => {
   if (max === undefined) {
-    max = min;
     return Math.floor(Math.random() * (max + 1));
   } else {
     return min + Math.floor(Math.random() * (max - min + 1));
