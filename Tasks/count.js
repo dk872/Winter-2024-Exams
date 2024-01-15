3aa2c815202540f1293350c07a5d2e6906b09482
dk872 <143122117+dk872@users.noreply.github.com>
2024-01-15 10:26:31 +0200

    one space removed

--- Tasks/count.js
+++ Tasks/count.js
@@ -5,7 +5,7 @@
 const count = (obj) => {
   let sum = 0;
   const keys = Object.keys(obj);
-   for (const key of keys) {
+  for (const key of keys) {
     const value = obj[key];
     if (typeof value === 'number') sum += value;
   }
