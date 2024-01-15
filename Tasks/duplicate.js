033da978688ca6483ff152773db6e78c33a1f522
dk872 <143122117+dk872@users.noreply.github.com>
2024-01-15 10:30:51 +0200

    add brackets for 'if'

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -3,8 +3,9 @@
 'use strict';
 
 const duplicate = (value, N) => {
-  if (N <= 0) return [];
-  else {
+  if (N <= 0) {
+    return [];
+  } else {
     const res = Array(N).fill(value);
     return res;
   }
